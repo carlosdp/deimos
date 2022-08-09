015937df19af22c78b5a611d8f279d63c09a1d90
Carlos Diaz-Padron <carlos@metaphor.xyz>
2022-08-09 10:47:04 -0400

    use refund pools

--- contracts/src/GovernorGasRefundProxy.sol
+++ contracts/src/GovernorGasRefundProxy.sol
@@ -6,50 +6,113 @@ import "@openzeppelin/contracts/governance/IGovernor.sol";
 
 error NoBudgetForRefund();
 error GasRefundFailed();
+error NoValueForPool();
+error NotPoolOwner();
+error ClosePoolFailed();
+error InvalidPool();
 
 contract GovernorGasRefundProxy {
-  mapping(address => uint256) balances;
-
-  uint256 constant APPROX_AUX_GAS = 3000;
-
-  event GovernorGasRefunded(address indexed target, address indexed voter, uint256 proposalId);
-
-  function fund(address _target) public payable {
-    // TODO: add ERC165 check to make sure contract compatible
-    uint256 balance = balances[_target];
-
-    balances[_target] = balance + msg.value;
+  struct RefundPool {
+    address owner;
+    uint256 balance;
+    uint256 maxFeePerGas;
+    uint256 maxPriorityFeePerGas;
   }
 
-  function castVoteBySig(address _target, uint256 _proposalId, uint8 _support, uint8 _v, bytes32 _r, bytes32 _s) public returns (uint256) {
+  mapping(address => mapping(uint256 => RefundPool)) refundPools;
+
+  uint256 constant REFUND_GAS_OVERHEAD = 3900;
+
+  event RefundPoolCreated(address indexed governor, uint256 indexed poolId, address indexed owner, uint256 balance);
+  event RefundPoolBalanceAdded(address indexed governor, uint256 indexed poolId, uint256 amountAdded);
+  event GovernorGasRefunded(address indexed governor, uint256 indexed poolId, address indexed voter, uint256 proposalId, uint256 amountRefunded);
+
+  function createPool(address _governor, uint256 _maxFeePerGas, uint256 _maxPriorityFeePerGas) public payable returns (uint256) {
+    if (msg.value == 0) {
+      revert NoValueForPool();
+    }
+
+    RefundPool memory pool;
+    pool.owner = msg.sender;
+    pool.balance = msg.value;
+    pool.maxFeePerGas = _maxFeePerGas;
+    pool.maxPriorityFeePerGas = _maxPriorityFeePerGas;
+
+    uint256 poolId = uint256(keccak256(abi.encode(msg.sender, _maxFeePerGas, _maxPriorityFeePerGas)));
+
+    refundPools[_governor][poolId] = pool;
+
+    emit RefundPoolCreated(_governor, poolId, msg.sender, pool.balance);
+
+    return poolId;
+  }
+
+  function closePool(address _governor, uint256 _poolId) public {
+    RefundPool storage pool = refundPools[_governor][_poolId];
+
+    if (pool.owner != msg.sender) {
+      revert NotPoolOwner();
+    }
+
+    uint256 poolBalance = pool.balance;
+    delete refundPools[_governor][_poolId];
+
+    (bool success, ) = payable(msg.sender).call{value: poolBalance}("");
+
+    if (!success) {
+      revert ClosePoolFailed();
+    }
+  }
+
+  function fund(address _governor, uint256 _poolId) public payable {
+    RefundPool storage pool = refundPools[_governor][_poolId];
+
+    if (pool.owner == address(0)) {
+      revert InvalidPool();
+    }
+
+    pool.balance += msg.value;
+  }
+
+  function castVoteBySig(address _governor, uint256 _poolId, uint256 _proposalId, uint8 _support, uint8 _v, bytes32 _r, bytes32 _s) public returns (uint256) {
     uint256 gasUsed = gasleft();
-    // execute vote
-    IGovernor governor = IGovernor(_target);
-    uint256 votes = governor.castVoteBySig(_proposalId, _support, _v, _r, _s);
+    uint256 votes;
 
-    require(votes > 0, "voter below minumum votes for refund");
+    {
+      // execute vote
+      IGovernor governor = IGovernor(_governor);
+      votes = governor.castVoteBySig(_proposalId, _support, _v, _r, _s);
 
-    uint256 priorityFee = tx.gasprice - block.basefee;
+      require(votes > 0, "voter below minumum votes for refund");
+    }
 
-    // if priority fee is >10%, we don't refund gas
-    if (priorityFee > block.basefee / 10) {
+    RefundPool storage pool = refundPools[_governor][_poolId];
+
+    // if fee is greater than pool max fee, we don't refund
+    if (pool.maxFeePerGas > 0 && tx.gasprice > pool.maxFeePerGas) {
       return votes;
     }
 
-    uint256 balance = balances[_target];
+    {
+      uint256 priorityFee = tx.gasprice - block.basefee;
 
-    emit GovernorGasRefunded(_target, msg.sender, _proposalId);
+      // if priority fee is greater than pool max priority fee, we don't refund
+      if (pool.maxPriorityFeePerGas > 0 && priorityFee > pool.maxPriorityFeePerGas) {
+        return votes;
+      }
+    }
 
     // refund gas (unless revert)
     gasUsed = gasUsed - gasleft();
-    uint256 gasCost = (gasUsed * tx.gasprice) + (APPROX_AUX_GAS * tx.gasprice);
-    // check if there is balance for the contract
-    if (balance < gasCost) {
-      revert NoBudgetForRefund();
+    uint256 gasCost = (gasUsed * tx.gasprice) + (REFUND_GAS_OVERHEAD * tx.gasprice);
+
+    // if there isn't enough to cover the gas cost, refund as much as we can
+    if (pool.balance < gasCost) {
+      gasCost = pool.balance;
     }
 
-    // deduct the gast cost from the contract balance before sending to user
-    balances[_target] = balance - gasCost;
+    // deduct the gas cost from the contract balance before sending to user
+    pool.balance = pool.balance - gasCost;
 
     // refund the gas cost to msg.sender
     (bool success, ) = payable(msg.sender).call{value: gasCost}("");
@@ -57,6 +120,8 @@ contract GovernorGasRefundProxy {
     if (!success) {
       revert GasRefundFailed();
     }
+
+    emit GovernorGasRefunded(_governor, _poolId, msg.sender, _proposalId, gasCost);
 
     return votes;
   }
--- contracts/test/GovernorGasRefundProxy.t.sol
+++ contracts/test/GovernorGasRefundProxy.t.sol
@@ -53,6 +53,121 @@ contract GovernorGasRefundProxyTest is Test {
       proxy = _proxy;
     }
 
+    function testRevertOnInvalidSignature() public {
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), 0, 0);
+
+      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, keccak256("hello world"));
+      vm.expectRevert("voter below minumum votes for refund");
+      proxy.castVoteBySig(address(governor), poolId, testProposalId, 1, _v, _r, _s);
+    }
+
+    function testSubmitsVote() public {
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), 0, 0);
+
+      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, testProposalId, uint8(1)))));
+      uint256 voteCount = proxy.castVoteBySig(address(governor), poolId, testProposalId, 1, _v, _r, _s);
+
+      require(governor.hasVoted(testProposalId, senderAddress), "vote not counted");
+      require(voteCount > 0, "vote count should be > 0");
+    }
+
+    function testRefundsTotalGasCost() public {
+      vm.fee(tx.gasprice);
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), 0, 0);
+
+      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // whole gas cost must be refunded
+      require(refund >= totalGasCost, "gas refund less than gas spent");
+
+      // gas refund shouldn't exceed approx +10% of total actual gas cost
+      require(refund <= totalGasCost + (totalGasCost/10), "gas refund greater than +10% total gas");
+    }
+
+    function testRefundsIfPriorityFeeBelowMaxPriorityFee() public {
+      _requireGasPriceIsSet();
+      vm.fee(tx.gasprice/2);
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), 0, tx.gasprice / 2);
+
+      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // whole gas cost must be refunded
+      require(refund >= totalGasCost, "gas refund less than gas spent");
+    }
+
+    function testDoesNotRefundIfPriorityFeeAboveMaxPriorityFee() public {
+      _requireGasPriceIsSet();
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), 0, tx.gasprice / 2 - 1);
+
+      (, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // no gas should be refunded
+      require(refund == 0, "gas refund should be 0");
+    }
+
+    function testRefundsIfFeeBelowMaxFee() public {
+      _requireGasPriceIsSet();
+      vm.fee(tx.gasprice/2);
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), tx.gasprice + 1, 0);
+
+      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // whole gas cost must be refunded
+      require(refund >= totalGasCost, "gas refund less than gas spent");
+    }
+
+    function testDoesNotRefundIfFeeAboveMaxFee() public {
+      _requireGasPriceIsSet();
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), tx.gasprice - 1, 0);
+
+      (, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // no gas should be refunded
+      require(refund == 0, "gas refund should be 0");
+    }
+
+    function testRefundsIfFeeBelowMaxFeeAndPriorityFeeBelowMax() public {
+      _requireGasPriceIsSet();
+      vm.fee(tx.gasprice/2);
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), tx.gasprice + 1, tx.gasprice / 2 + 1);
+
+      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // whole gas cost must be refunded
+      require(refund >= totalGasCost, "gas refund less than gas spent");
+    }
+
+    function testDoesNotRefundIfFeeAboveMaxFeeAndPriorityFeeBelowMaxFee() public {
+      _requireGasPriceIsSet();
+
+      uint256 poolId = proxy.createPool{value: 0.5 ether}(address(governor), tx.gasprice - 1, tx.gasprice / 2);
+
+      (, uint256 refund) = _measureCastVoteGas(poolId);
+
+      // no gas should be refunded
+      require(refund == 0, "gas refund should be 0");
+    }
+
+    function _measureCastVoteGas(uint256 _poolId) internal returns (uint256 totalGasCost, uint256 refund) {
+      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, testProposalId, uint8(1)))));
+
+      uint256 balanceBefore = senderAddress.balance;
+      uint256 gasLeftBeforeCall = gasleft();
+
+      proxy.castVoteBySig(address(governor), _poolId, testProposalId, 1, _v, _r, _s);
+
+      uint256 gasLeftAfterCall = gasleft();
+      uint256 totalGasUsed = gasLeftBeforeCall - gasLeftAfterCall - 8109;
+      uint256 balanceAfter = senderAddress.balance;
+      refund = balanceAfter - balanceBefore;
+      totalGasCost = totalGasUsed * tx.gasprice;
+    }
+
     function _hashTypedDataV4(bytes32 structHash) internal view returns (bytes32) {
       bytes32 typeHash = keccak256(
           "EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)"
@@ -63,69 +178,7 @@ contract GovernorGasRefundProxyTest is Test {
       return ECDSA.toTypedDataHash(keccak256(abi.encode(typeHash, nameHash, versionHash, block.chainid, address(governor))), structHash);
     }
 
-    function testRevertOnInvalidSignature() public {
-      proxy.fund{value: 0.5 ether}(address(governor));
-
-      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, keccak256("hello world"));
-      vm.expectRevert("voter below minumum votes for refund");
-      proxy.castVoteBySig(address(governor), testProposalId, 1, _v, _r, _s);
-    }
-
-    function testSubmitsVote() public {
-      proxy.fund{value: 0.5 ether}(address(governor));
-
-      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, testProposalId, uint8(1)))));
-      uint256 voteCount = proxy.castVoteBySig(address(governor), testProposalId, 1, _v, _r, _s);
-
-      require(governor.hasVoted(testProposalId, senderAddress), "vote not counted");
-      require(voteCount > 0, "vote count should be > 0");
-    }
-
-    function testRefundsTotalGasCost() public {
-      vm.fee(tx.gasprice);
-      proxy.fund{value: 0.5 ether}(address(governor));
-
-      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas();
-
-      // whole gas cost must be refunded
-      require(refund >= totalGasCost, "gas refund less than gas spent");
-
-      // gas refund shouldn't exceed approx +10% of total actual gas cost
-      require(refund <= totalGasCost + (totalGasCost/10), "gas refund greater than +10% total gas");
-    }
-
-    function testRefundsIfPriorityFeeBelow10PercentOfBaseFee() public {
-      vm.fee((tx.gasprice/100)*91);
-      proxy.fund{value: 0.5 ether}(address(governor));
-
-      (uint256 totalGasCost, uint256 refund) = _measureCastVoteGas();
-
-      // whole gas cost must be refunded
-      require(refund >= totalGasCost, "gas refund less than gas spent");
-    }
-
-    function testDoesNotRefundIfPriorityFeeAbove10PercentOfBaseFee() public {
-      vm.fee((tx.gasprice/10)*8);
-      proxy.fund{value: 0.5 ether}(address(governor));
-
-      (, uint256 refund) = _measureCastVoteGas();
-
-      // no gas should be refunded
-      require(refund == 0, "gas refund should be 0");
-    }
-
-    function _measureCastVoteGas() internal returns (uint256 totalGasCost, uint256 refund) {
-      (uint8 _v, bytes32 _r, bytes32 _s) = vm.sign(senderPrivateKey, _hashTypedDataV4(keccak256(abi.encode(BALLOT_TYPEHASH, testProposalId, uint8(1)))));
-
-      uint256 balanceBefore = senderAddress.balance;
-      uint256 gasLeftBeforeCall = gasleft();
-
-      proxy.castVoteBySig(address(governor), testProposalId, 1, _v, _r, _s);
-
-      uint256 gasLeftAfterCall = gasleft();
-      uint256 totalGasUsed = gasLeftBeforeCall - gasLeftAfterCall - 8109;
-      uint256 balanceAfter = senderAddress.balance;
-      refund = balanceAfter - balanceBefore;
-      totalGasCost = totalGasUsed * tx.gasprice;
+    function _requireGasPriceIsSet() internal view {
+      require(tx.gasprice > 0, "must set gas price when running these tests using --gas-price flag");
     }
 }
